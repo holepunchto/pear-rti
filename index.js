67d3a8774a803456d48d74e2255605cfd84532cf
Mikkel Malmberg <mikkel@brnbw.com>
2026-01-05 11:43:40 +0100

    Migrate to Prettier (#1)

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 const CHECKOUT = global.Pear?.constructor.RTI.checkout
 
 if (!CHECKOUT) {
-  throw new Error('pear-rti: missing bootstrap runtime information. Pear.constructor.RTI.checkout must be set.')
+  throw new Error(
+    'pear-rti: missing bootstrap runtime information. Pear.constructor.RTI.checkout must be set.'
+  )
 }
 
-let mount = global.Pear?.constructor.RTI.mount ? toURL(global.Pear?.constructor.RTI.mount + '/', 'file:') : null
+let mount = global.Pear?.constructor.RTI.mount
+  ? toURL(global.Pear?.constructor.RTI.mount + '/', 'file:')
+  : null
 if (!mount) {
   let url = require.main?.url
   if (url?.href.endsWith('/boot.bundle')) url.href += '/'
@@ -17,7 +21,7 @@ if (!mount) {
 exports.CHECKOUT = CHECKOUT
 exports.MOUNT = mount
 
-function toURL (s) {
+function toURL(s) {
   if (s[1] === ':' && s[0] !== '/') return new URL('file:' + s)
   if (s.startsWith('file:')) return new URL(s)
   return new URL(s, 'file:')
